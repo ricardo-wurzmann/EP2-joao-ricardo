f7ceccc2155f1e5ae6ea01fa426035f4a29008fc
ricardo-wurzmann <ricardow3@al.insper.edu.br>
2022-05-10 14:28:55 -0300

    inicial

--- Jogo final.py
+++ Jogo final.py
@@ -3,3 +3,16 @@ import Funcoes
 import Base_países
 dados=Funcoes.normaliza(Base_países.DADOS)
 print(dados)
+
+print(" ============================")
+print("|                            |")
+print("| Bem-vindo ao Insper Países |")
+print("|                            |")
+print(" ==== Design de Software ==== ")
+print(" Comandos:")
+print("    dica       - entra no mercado de dicas")
+print("    desisto    - desiste da rodada")
+print("    inventario - exibe sua posição")
+print("Um país foi escolhido, tente adivinhar!")
+print("Você tem tentativa(s)")
+print("Qual seu palpite?")
